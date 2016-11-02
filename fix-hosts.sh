44b660430f778e322634825f6554b6db6a1fcb1d
devshawn <musicalshawn@gmail.com>
2016-11-01 19:27:26 -0500

    Removing things I should not have commited

--- fix-hosts.sh
+++ fix-hosts.sh
@@ -5,6 +5,6 @@ LDAP_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{
 LDAP2_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ldapdockerloadbalancer_ldap-master-b_1)
 
 docker exec $LDAP_CID bash -c "echo $LDAP2_IP ldap2.example.org >> /etc/hosts"
+docker exec $LDAP2_CID bash -c "echo $LDAP2_IP ldap2.example.org >> /etc/hosts"
+docker exec $LDAP_CID bash -c "echo $LDAP_IP ldap.example.org >> /etc/hosts"
 docker exec $LDAP2_CID bash -c "echo $LDAP_IP ldap.example.org >> /etc/hosts"
-
-ldapadd -x -D "cn=admin,dc=example,dc=org" -w test -f /container/service/slapd/assets/test/new-user.ldif -h ldap.example.org -ZZ
